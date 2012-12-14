241abc615d56a4653146f8ee193d48d86201adc9
Steph <nivot.stephane@hotmail.fr>
2012-12-14 12:30:44 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IFPSProject::Application.config.secret_token = 'e3ed9dcf300a73c123ec3f3e491e90d553dc804dd81b2d8061c6f3e3727ecea74303681e1097cf51ce8a12b7c65a248b67efd86b17ae03432ea31384e454cc0c'
